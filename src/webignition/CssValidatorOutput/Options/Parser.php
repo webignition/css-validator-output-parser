cea1371d77f432d9b24a5ed571108e39e9849d32
Jon Cram <jon@webignition.net>
2012-12-27 17:37:25 +0000

    Clear parsed options when new output is set

--- src/webignition/CssValidatorOutput/Options/Parser.php
+++ src/webignition/CssValidatorOutput/Options/Parser.php
@@ -24,6 +24,7 @@ class Parser {
      */
     public function setOptionsOutput($optionsOutput) {
         $this->optionsOutput = $optionsOutput;
+        $this->options = null;
         return $this;
     }
     
