c5894cb2135f634ef56421456a2392cddd99a44f
Jon Cram <jon@webignition.net>
2013-12-31 11:26:29 +0000

    Move further exception-related tests

--- tests/webignition/Tests/CssValidatorOutput/Parser/GetOutput/ExceptionOutputTest.php
+++ tests/webignition/Tests/CssValidatorOutput/Parser/GetOutput/ExceptionOutputTest.php
@@ -117,5 +117,67 @@ class ExceptionOutputTest extends BaseTest {
         $this->assertEquals(0, $cssValidatorOutput->getMessageCount());
         $this->assertEquals(0, $cssValidatorOutput->getErrorCount());
         $this->assertEquals(0, $cssValidatorOutput->getWArningCount());      
+    } 
+    
+    
+    public function testParseUnknownMimeTypeError() {
+        $rawOutput = $this->getFixture('Exception/java.io.IOException.unknownmimetype.txt');
+        
+        $parser = new Parser();
+        $parser->setRawOutput($rawOutput);
+        
+        $cssValidatorOutput = $parser->getOutput();                
+        
+        $this->assertInstanceOf('webignition\CssValidatorOutput\CssValidatorOutput', $cssValidatorOutput);
+        
+        $this->assertTrue($cssValidatorOutput->getIsUnknownMimeTypeError());
+        
+        $options = $cssValidatorOutput->getOptions();        
+        $this->assertInstanceOf('webignition\CssValidatorOutput\Options\Options', $options);
+
+        $this->assertFalse($options->getVendorExtensionIssuesAsWarnings());
+        $this->assertEquals('ucn', $options->getOutputFormat());        
+        $this->assertEquals('en', $options->getLanguage());
+        $this->assertEquals(2, $options->getWarningLevel());
+        $this->assertEquals('all', $options->getMedium());
+        $this->assertEquals('css3', $options->getProfile());         
+
+        $datetime = $cssValidatorOutput->getDateTime();
+        $this->assertNull($datetime);
+        
+        $this->assertEquals(0, $cssValidatorOutput->getMessageCount());
+        $this->assertEquals(0, $cssValidatorOutput->getErrorCount());
+        $this->assertEquals(0, $cssValidatorOutput->getWArningCount());      
+    }    
+    
+    
+    public function testParseUnknownHostErrorOutput() {
+        $rawOutput = $this->getFixture('Exception/java.net.UnknownHostException.txt');
+        
+        $parser = new Parser();
+        $parser->setRawOutput($rawOutput);
+        
+        $cssValidatorOutput = $parser->getOutput();                
+        
+        $this->assertInstanceOf('webignition\CssValidatorOutput\CssValidatorOutput', $cssValidatorOutput);        
+        
+        $this->assertTrue($cssValidatorOutput->getIsUnknownHostErrorOutput());
+        
+        $options = $cssValidatorOutput->getOptions();        
+        $this->assertInstanceOf('webignition\CssValidatorOutput\Options\Options', $options);
+
+        $this->assertFalse($options->getVendorExtensionIssuesAsWarnings());
+        $this->assertEquals('ucn', $options->getOutputFormat());        
+        $this->assertEquals('en', $options->getLanguage());
+        $this->assertEquals(2, $options->getWarningLevel());
+        $this->assertEquals('all', $options->getMedium());
+        $this->assertEquals('css3', $options->getProfile());
+        
+        $datetime = $cssValidatorOutput->getDateTime();
+        $this->assertNull($datetime);
+        
+        $this->assertEquals(0, $cssValidatorOutput->getMessageCount());
+        $this->assertEquals(0, $cssValidatorOutput->getErrorCount());
+        $this->assertEquals(0, $cssValidatorOutput->getWArningCount());      
     }    
 }
