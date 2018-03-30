b899a06ae63ebfde763f040b21d43f180aa92af4
Jon Cram <jon@webignition.net>
2018-03-30 07:32:01 +0100

    Test ExceptionOutput/ExceptionOutput

--- src/ExceptionOutput/ExceptionOutput.php
+++ src/ExceptionOutput/ExceptionOutput.php
@@ -4,103 +4,93 @@ namespace webignition\CssValidatorOutput\ExceptionOutput;
 
 use webignition\CssValidatorOutput\ExceptionOutput\Type\Type;
 
-class ExceptionOutput {
-
+class ExceptionOutput
+{
     /**
-     *
      * @var Type
      */
-    private $type = null;    
-    
-    
+    private $type = null;
+
     /**
-     * 
      * @param Type $type
      */
-    public function setType(Type $type) {
+    public function setType(Type $type)
+    {
         $this->type = $type;
     }
-    
-    
+
     /**
-     * 
      * @param string $name
      * @param array $arguments Not Used
-     * @return boolean
+     *
+     * @return bool
      */
-    public function __call($name, $arguments) {        
+    public function __call($name, $arguments)
+    {
         return $this->type->get() == str_replace('is', '', strtolower($name));
     }
-    
-    
+
     /**
-     * 
-     * @return \webignition\CssValidatorOutput\ExceptionOutput\Type\Type
+     * @return Type
      */
-    public function getType() {
+    public function getType()
+    {
         return $this->type;
     }
-    
-    
+
     /**
-     * 
-     * @return boolean
+     * @return bool
      */
-    public function isHttpError() {
+    public function isHttpError()
+    {
         return substr($this->type->get(), 0, strlen('http')) === 'http';
     }
-    
-    
+
     /**
-     * 
-     * @return boolean
+     * @return bool
      */
-    public function isHttpClientError() {
+    public function isHttpClientError()
+    {
         return substr($this->type->get(), 0, strlen('http4')) === 'http4';
     }
-    
-    
+
     /**
-     * 
-     * @return boolean
+     * @return bool
      */
-    public function isHttpServerError() {
+    public function isHttpServerError()
+    {
         return substr($this->type->get(), 0, strlen('http5')) === 'http5';
     }
-    
-    
+
     /**
-     * 
      * @return int
      */
-    public function getHttpStatusCode() {
+    public function getHttpStatusCode()
+    {
         if (!$this->isHttpError()) {
             return null;
         }
-        
+
         return (int)str_replace('http', '', $this->type->get());
     }
-    
-    
+
     /**
-     * 
-     * @return boolean
+     * @return bool
      */
-    public function isCurlError() {
+    public function isCurlError()
+    {
         return substr($this->type->get(), 0, strlen('curl')) === 'curl';
     }
-    
-    
+
     /**
-     * 
      * @return int
      */
-    public function getCurlCode() {
+    public function getCurlCode()
+    {
         if (!$this->isCurlError()) {
             return null;
         }
-        
-        return (int)str_replace('curl', '', $this->type->get());        
+
+        return (int)str_replace('curl', '', $this->type->get());
     }
-    
 }
