a8d1f5adb13ec1097788e7b17df636a2a846804c
Jon Cram <webignition@gmail.com>
2018-04-10 15:59:49 +0100

    Implement AbstractMessage::jsonSerialize(); add Message/Factory::createFromArray() (#2)

--- src/Message/Factory.php
+++ src/Message/Factory.php
@@ -4,9 +4,6 @@ namespace webignition\CssValidatorOutput\Message;
 
 class Factory
 {
-    const TYPE_ERROR = 'error';
-    const TYPE_WARNING = 'warning';
-
     /**
      * @param \DOMElement $messageElement
      *
@@ -16,20 +13,19 @@ class Factory
     {
         $type = $messageElement->getAttribute('type');
 
-        if (self::TYPE_ERROR !== $type && self::TYPE_WARNING !== $type) {
+        if (AbstractMessage::TYPE_ERROR !== $type && AbstractMessage::TYPE_WARNING !== $type) {
             return null;
         }
 
         $contextNode = $messageElement->getElementsByTagName('context')->item(0);
 
-        $ref = $messageElement->getAttribute('ref');
-        $context = $contextNode->nodeValue;
-        $lineNumber = $contextNode->getAttribute('line');
-        $message = trim($messageElement->getElementsByTagName('title')->item(0)->nodeValue);
-
-        return self::TYPE_ERROR === $type
-            ? new Error($message, $context, $ref, $lineNumber)
-            : new Warning($message, $context, $ref, $lineNumber);
+        return self::createFromArray([
+            AbstractMessage::KEY_TYPE => $type,
+            AbstractMessage::KEY_MESSAGE => trim($messageElement->getElementsByTagName('title')->item(0)->nodeValue),
+            AbstractMessage::KEY_CONTEXT => $contextNode->nodeValue,
+            AbstractMessage::KEY_REF => $messageElement->getAttribute('ref'),
+            AbstractMessage::KEY_LINE_NUMBER => $contextNode->getAttribute('line'),
+        ]);
     }
 
     /**
@@ -39,11 +35,21 @@ class Factory
      */
     public static function createWarningFromError(Error $error)
     {
-        return new Warning(
-            $error->getMessage(),
-            $error->getContext(),
-            $error->getRef(),
-            $error->getLineNumber()
-        );
+        return self::createFromArray(array_merge($error->jsonSerialize(), [
+            AbstractMessage::KEY_TYPE => AbstractMessage::TYPE_WARNING,
+        ]));
+    }
+
+    public static function createFromArray(array $messageData)
+    {
+        $type = $messageData[AbstractMessage::KEY_TYPE];
+        $message  = $messageData[AbstractMessage::KEY_MESSAGE];
+        $context  = $messageData[AbstractMessage::KEY_CONTEXT];
+        $ref  = $messageData[AbstractMessage::KEY_REF];
+        $lineNumber  = $messageData[AbstractMessage::KEY_LINE_NUMBER];
+
+        return AbstractMessage::TYPE_ERROR === $type
+            ? new Error($message, $context, $ref, $lineNumber)
+            : new Warning($message, $context, $ref, $lineNumber);
     }
 }
